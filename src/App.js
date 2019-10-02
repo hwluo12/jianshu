7807aecdaf77b5dac1a6084d32f2c7b789818e30
hwluo12 <hwluo12@163.com>
2019-10-02 08:43:21 +0800

    项目目录搭建，styled-components与reset.css的结合使用

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+/*
+ * @Description: app.js
+ * @Author: hwluo
+ * @Date: 2019-10-02 08:06:24
+ * @LastEditTime: 2019-10-02 08:10:49
+ * @LastEditors: hwluo
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dell'>
+      hello world
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
+/*
+ * @Description: index.js
+ * @Author: hwluo
+ * @Date: 2019-10-02 08:06:24
+ * @LastEditTime: 2019-10-02 08:42:00
+ * @LastEditors: hwluo
+ */
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        line-height: 1;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppContainer = (
+    <Fragment>
+        <App />
+        <GlobalStyle />
+    </Fragment>
+)
+
+ReactDOM.render(AppContainer, document.getElementById('root'));
